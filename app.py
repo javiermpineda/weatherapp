309a6758b50b563cb39055da0376816cc72ed0c4
javiermpineda <javier_mpineda@hotmail.com>
2023-11-14 10:50:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,21 +3,24 @@ import requests
 
 app=Flask(__name__)
 
-@app.route('/weatherapp',methods=["POST","GET"])
-def get_weatherdata():
+@app.route('/')
+def homepage():
     return render_template("index.html")
 
-def get_weather_data():
-    url= 'https://api.openweathermap.org/data/2.5/weather'
+@app.route("/weatherapp", methods=["POST", "GET"])
+def get_weatherdata():
+    url="https://api.openweathermap.org/data/2.5/weather"
 
-    param={'q':request.form.get("city"),
-       'units':request.form.get("units"),
-       'appid':request.form.get("appid")}
-
-    response=requests.get(url,params=param)
-    data=response.json()
+    params ={
+        'q':request.form.get("city"),
+        'appid':request.form.get('appid'),
+        'units':request.form.get('units')
+    }
+    response=requests.get(url, params=params)
+    data = response.json()
     city=data['name']
-    return f"data: {data} , city:{city}"
+    return f"data : {data}, city:{city}"
 
-if __name__== "__main__":
-    app.run(host="0.0.0.0", port=5004)
+
+if __name__=='__main__':
+    app.run(host="0.0.0.0", port=5000)
